d30c96eb178951c56b123b16081f2383e2fd146c
Roberto Carlos Tapia De León <rocatale@ciencias.unam.mx>
2020-09-10 21:34:54 -0500

    Commit 1 Clase 2

---
+++ app/src/main/java/com/curso/claseandroid1/Clase1.java
@@ -0,0 +1,25 @@
+package com.curso.claseandroid1;
+
+public class Clase1 {
+
+    // Campos
+    String string;
+    int i;
+    float f;
+
+    // Constructor
+
+    public Clase1(String string) {
+        this.string = string;
+    }
+
+    // Metodos
+
+    public int suma(int a, int b){
+        int c;
+        c = a+b;
+        return c;
+    }
+
+
+}
--- app/src/main/java/com/curso/claseandroid1/MainActivity.java
+++ app/src/main/java/com/curso/claseandroid1/MainActivity.java
@@ -1,16 +1,27 @@
 package com.curso.claseandroid1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.Button;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button boton;
+    Clase1 clase1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String cadena = "Hola";
+        int j = 10;
+        clase1 = new Clase1(cadena);
+
+        int a= 2;
+        int b = 5;
+        int c = clase1.suma(a,b);
+
+        Log.d("TagSuma", Integer.toString(c));
+
     }
 }
