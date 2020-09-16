07ac831270ba7d4f16ab0606167edf2f46ebf2ff
Roberto Carlos Tapia De León <rocatale@ciencias.unam.mx>
2020-09-15 22:07:47 -0500

    Commit 2 Clase 5

--- app/src/main/java/com/curso/claseandroid1/Activity2.java
+++ app/src/main/java/com/curso/claseandroid1/Activity2.java
@@ -7,12 +7,15 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,44 +28,72 @@ public class Activity2 extends AppCompatActivity {
     //Campos
     private int codigoDeSolicitud = 1;
     private FusedLocationProviderClient fusedLocationClient;
-    private String hola= "Hola";
+    private String hola = "Hola";
     private String[] arregloDeCadenas;
     private int[] arregloDeEnteros;
+    private Button bPosicion;
 
     TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2);
 
-        arregloDeCadenas = new String[]{ "a", "b", "c"};
+        arregloDeCadenas = new String[]{"a", "b", "c"};
 
+        bPosicion = findViewById(R.id.buttonposicion);
         textView = findViewById(R.id.textView2);
+
         Intent intent = getIntent();
         String cadenaRecuperada = intent.getStringExtra("cadena");
 
         textView.setText(cadenaRecuperada);
 
         //Inicializar FusedLocationClient
-
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            pedriPermiso();
-        }
-
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
 
-        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            Log.d("Coordenadas", location.toString());
-                        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            pedriPermiso();
+        } else {
+            fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    // Got last known location. In some rare situations this can be null.
+                    if (location != null) {
+                        Log.d("Coordenadas", location.toString());
                     }
-                });
+                    else{
+                        Log.d("Coordenadas", "Coordenas nulas");
+
+                    }
+                }
+            });
+        }
+
+        bPosicion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                solicitarUltimaPosicion();
+            }
+        });
 
 
+    }
+
+    private void solicitarUltimaPosicion() {
+
+
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+                    Log.d("Coordenadas", location.toString());
+                }
+            }
+        });
     }
 
     private void pedriPermiso() {
@@ -79,6 +110,7 @@ public class Activity2 extends AppCompatActivity {
 
         if(requestCode == codigoDeSolicitud && grantResults[0] == PackageManager.PERMISSION_GRANTED){
             Toast.makeText(this, "Permiso Permitido",Toast.LENGTH_SHORT).show();
+            solicitarUltimaPosicion();
         }
     }
 }
